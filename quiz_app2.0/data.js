3435b8e899b26d44a2f38e4a5ce14831e7ef1066
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-05-02 22:36:25 +0200

    data.js for json array

---
+++ quiz_app2.0/data.js
@@ -0,0 +1,77 @@
+let quiz = [
+    {
+        'name': 'HTML_Quiz',
+        'questions': [{
+            'question_0': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        },
+        {
+            'question_1': 'test',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        }
+        ]
+
+
+
+    },
+    {
+        'name': 'CSS_Quiz',
+        'questions': [{
+            'question_0': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+
+        },
+
+        {
+            'question_1': 'test',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        }
+
+
+
+        ]
+    },
+    {
+        'name': 'JS_Quiz',
+        'questions': [{
+            'question_0': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        },
+        {
+
+            'question_1': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+
+
+        }
+
+
+        ]
+
+    },
+    {
+        'name': 'Java_Quiz',
+        'questions': [{
+            'question_0': 'java',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        },
+        {
+
+            'question_1': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+
+
+        }
+
+
+        ]
+    }
+];
