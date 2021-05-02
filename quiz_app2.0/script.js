34892c9c9560e27bf4d3f4a57d357c00e19dec7e
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-05-02 04:09:14 +0200

    update-function

--- quiz_app2.0/script.js
+++ quiz_app2.0/script.js
@@ -1,19 +1,86 @@
-let Quiz = [
+let quiz = [
     {
         'name': 'HTML_Quiz',
+        'questions': [{
+            'question_0': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        },
+        {
+            'question_1': 'test',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        }
+        ]
+
+
+
     },
     {
         'name': 'CSS_Quiz',
+        'questions': [{
+            'question_0': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+
+        },
+
+        {
+            'question_1': 'test',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        }
+
+
+
+        ]
     },
     {
         'name': 'JS_Quiz',
+        'questions': [{
+            'question_0': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        },
+        {
+
+            'question_1': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+
+
+        }
+
+
+        ]
 
     },
     {
         'name': 'Java_Quiz',
+        'questions': [{
+            'question_0': 'java',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+        },
+        {
+
+            'question_1': 'Wer hat HTML erfunden',
+            'answer': ['Robbie Williams', 'Lady Gaga', 'Tim Berners - Lee', 'Justin Bieber'],
+            'right-answer': 2,
+
+
+        }
+
+
+        ]
     }
 ];
 
+let answerInLetters = ['A', 'B', 'C', 'D']
+let questNumber = 0;
+let currentQuiz;
+let score = 0;
+let progress = 0;
 
 function OpenNewQuiz(id) {
 
@@ -21,7 +88,7 @@ function OpenNewQuiz(id) {
     <img src="img/brainbg.jpg">
 
             <div id="start-text">
-                <h1>Welcome to <br> The Awesome ${Quiz[id]['name']}</h1>
+                <h1>Welcome to <br> The Awesome ${quiz[id]['name']}</h1>
                 <h2>Ready for the Challange?</h2>
             </div>
 
@@ -34,49 +101,93 @@ function OpenNewQuiz(id) {
 
 }
 
-
 function StartNow(id) {
+    currentQuiz = quiz[id];
+    update()
+}
+
+
+function update() {
+
+    let currentQuestion = currentQuiz['questions'][questNumber];
+    document.getElementById('progress-bar').style = `width: ${progress}%`;
 
     document.getElementById('container-question').innerHTML = `
 
         <img src="img/brainbg.jpg">
 
-
-
         <div  class="question-text">
             <h2>
-                Wer hat HTML erfunden?
+                ${currentQuestion['question_' + questNumber]}
             </h2>
 
-
-
-            <div class="answer-container-A">
-                <button onclick="answer(A)" class="btn btn-primary">A</button>
-                <h3>Robbie Williams</h3>
-            </div>
-
-            <div class="answer-container-B">
-                <button onclick="answer(B)" class="btn btn-primary">B</button>
-                <h3>Lady Gaga</h3>
-            </div>
-
-            <div class="answer-container-C">
-                <button onclick="answer(C)" class="btn btn-primary">C</button>
-                <h3>Tim Berners - Lee</h3>
-            </div>
-
-            <div class="answer-container-D">
-                <button onclick="answer(D)" class="btn btn-primary">D</button>
-                <h3>Justin Bieber</h3>
-            </div>
-
-
+      ${showAnswerContainer(currentQuestion)}
+        
             <div class="button-game">
-                <button onclick="previousQuestion()"> &#171; </button>
-                <button onclick="nextQuestion()">&#187; </button>
+                <button  id="previousQuestion" onclick="previousQuestion()"> &#171; </button>
+                <button id="nextQuestion" class="d-none" onclick="nextQuestion()">&#187; </button>
             </div>
 
         </div>
 
         `;
+}
+
+function showAnswerContainer(currentQuestion) {
+    let answerRow = `<div>`
+
+    for (let i = 0; i < currentQuestion['answer'].length; i++) {
+        const answer = currentQuestion['answer'][i];
+
+        answerRow += ` 
+        <div id=${i} class="answer-container">
+        <button id="test${i}"  onclick="checkAnswer(${i}); " class="btn btn-primary">${answerInLetters[i]}</button>
+        <h3>${answer}</h3>
+        </div>`
+    }
+
+    answerRow += `</div>`
+    return answerRow
+}
+
+
+function checkAnswer(i) {
+    document.getElementById('test' + i).disabled = true;
+    let answer = currentQuiz['questions'][questNumber]['right-answer']
+    console.log(i)
+
+    if (answer == i) {
+        //right answer
+        score++;
+        document.getElementById(i).classList.add('right-answer')
+        document.getElementById('nextQuestion').classList.remove('d-none')
+
+    } else {
+        //wrong answer
+        document.getElementById(i).classList.add('wrong-answer')
+        document.getElementById('nextQuestion').classList.remove('d-none')
+
+    }
+
+
+}
+
+function nextQuestion() {
+    questNumber++
+    calculateProgress()
+    update()
+}
+
+function previousQuestion() {
+    questNumber--;
+
+    if (questNumber < 0) {
+        questNumber = 0;
+    }
+    update();
+}
+
+function calculateProgress() {
+    quiz_length = currentQuiz['questions'].length;
+    progress = ((100 / quiz_length) * questNumber);
 }
